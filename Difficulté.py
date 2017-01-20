ab9e33682f4aa4cd9dc89f0e0a5bc974bebc280c
Quenntinne <wilbauxq@gmail.com>
2017-01-20 23:06:19 +0100

    Add files via upload
    Les bout de code selon la difficulté choisie

---
+++ Difficulté.py
@@ -0,0 +1,26 @@
+print("- signifie que le chiffre se trouve dans le code mais pas a cette place \n * signifie que le chiffre n'est pas dans le code")
+if d==3 :
+    for i in range(n) :
+        for j in range(n) :
+            if liste[i]==code[j] :
+                good=good+1
+elif d==2 :
+    for i in range(n) :
+        for j in range(n) :
+            if liste[i]==code[j] :
+                if i==j :
+                    good=good+1
+                else :
+                    almostgood=almostgood+1
+elif d==1 :
+    listebon=list(range(n))
+    for i in range(n) :
+        for j in range(n) :
+            if liste[i]==code[j] :
+                if i==j :
+                    good=good+1
+                    listebon[i]=1
+                else :
+                    almostgood=almostgood+1
+                    listebon[i]=2
+                
